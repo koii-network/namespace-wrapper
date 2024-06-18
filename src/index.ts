58e6c11c551cdd7e677b60d4744ce13717f3fff7
Raj Prajapati <rajce604@gmail.com>
2024-06-18 12:47:43 -0400

    submission wrapper added.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { createHash } from 'crypto'
-import { Connection, Keypair, PublicKey } from '@_koi/web3.js'
+import { Connection, Keypair } from '@_koi/web3.js'
 import Datastore from 'nedb-promises'
 import { promises as fsPromises } from 'fs'
 import bs58 from 'bs58'
@@ -8,7 +8,7 @@ import nacl from 'tweetnacl'
 import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
-import { Document } from './types'
+import { GenericHandlerResponse, TaskState, TaskNode } from './types'
 
 dotenv.config()
 
@@ -53,135 +53,154 @@ const BASE_ROOT_URL: string = `http://localhost:${TASK_NODE_PORT}/namespace-wrap
 
 let connection: Connection
 
-class NamespaceWrapper {
-  //public db: Datastore<Document> | null = null
-
-  #db: any
-  #testingMainSystemAccount: Keypair | null = null
-  #testingStakingSystemAccount: Keypair | null = null
-  #testingDistributionList: any = null
-  #testingTaskState: any = null
+class NamespaceWrapper implements TaskNode {
+  private db: Datastore<any> | null = null
+  public testingMainSystemAccount: Keypair | null = null
+  public testingStakingSystemAccount: Keypair | null = null
+  public testingTaskState: TaskState | null = null
+  public testingDistributionList: any = null
 
   constructor() {
     if (taskNodeAdministered) {
       this.initializeDB()
     } else {
-      this.#db = Datastore.create('./localKOIIDB.db')
+      this.db = Datastore.create('./localKOIIDB.db')
       this.defaultTaskSetup()
     }
   }
 
   public async initializeDB(): Promise<void> {
-    if (this.#db) return
+    if (this.db) return
     try {
       if (taskNodeAdministered) {
         const path = await this.getTaskLevelDBPath()
-        this.#db = Datastore.create(path)
+        this.db = Datastore.create(path)
       } else {
-        this.#db = Datastore.create('./localKOIIDB.db')
+        this.db = Datastore.create('./localKOIIDB.db')
       }
     } catch (e) {
-      this.#db = Datastore.create(`../namespace/${TASK_ID}/KOIILevelDB.db`)
+      this.db = Datastore.create(`../namespace/${TASK_ID}/KOIILevelDB.db`)
     }
   }
 
-  async getDb(): Promise<Datastore<Document>> {
-    if (this.#db) return this.#db
+  async getDb(): Promise<Datastore<any>> {
+    if (this.db) return this.db
     await this.initializeDB()
-    return this.#db!
+    return this.db!
   }
 
-  /**
-   * Namespace wrapper of storeGetAsync
-   * @param {string} key // Path to get
-   * @returns {Promise<*>} Promise containing data
-   */
   async storeGet(key: string): Promise<string | null> {
     try {
       await this.initializeDB()
-      const resp = await this.#db.findOne({ key: key })
-      if (resp) {
-        return resp[key]
-      } else {
-        return null
-      }
+      const resp = await this.db!.findOne({ key })
+      return resp ? resp[key] : null
     } catch (e) {
       console.error(e)
       return null
     }
   }
 
-  /**
-   * Namespace wrapper over storeSetAsync
-   * @param {string} key Path to set
-   * @param {*} value Data to set
-   * @returns {Promise<void>}
-   */
   async storeSet(key: string, value: string): Promise<void> {
     try {
       await this.initializeDB()
-      await this.#db.update(
-        { key: key },
-        { [key]: value, key },
-        { upsert: true },
-      )
+      await this.db!.update({ key }, { [key]: value, key }, { upsert: true })
     } catch (e) {
       console.error(e)
       return undefined
     }
   }
 
+  async getSlot(): Promise<number> {
+    if (taskNodeAdministered) {
+      const response = await genericHandler('getCurrentSlot')
+      if (typeof response === 'number') {
+        return response
+      } else {
+        console.error('Error getting slot:', response)
+        return 0 // or handle error appropriately
+      }
+    } else {
+      return 100
+    }
+  }
+
+  async checkSubmissionAndUpdateRound(
+    submissionValue: string = 'default',
+    round: number,
+  ): Promise<void> {
+    if (taskNodeAdministered) {
+      await genericHandler(
+        'checkSubmissionAndUpdateRound',
+        submissionValue,
+        round.toString(),
+      )
+    } else {
+      if (!this.testingTaskState!.submissions[round]) {
+        this.testingTaskState!.submissions[round] = {}
+      }
+      this.testingTaskState!.submissions[round][
+        this.testingStakingSystemAccount!.publicKey.toBase58()
+      ] = {
+        submission_value: submissionValue,
+        slot: 100,
+        round,
+      }
+    }
+  }
+
+  async getTaskState(options: any): Promise<any> {
+    if (taskNodeAdministered) {
+      const response = await genericHandler('getTaskState', options)
+      if (typeof response === 'number') {
+        // Handle error response (numbers)
+        console.log('Error in getting task state', response)
+        return null
+      } else {
+        // Handle successful response
+        return response
+      }
+    } else {
+      return this.testingTaskState
+    }
+  }
+
   async getRpcUrl(): Promise<string> {
-    // Implement this method based on your application logic
     return K2_NODE_URL
   }
 
   async getTaskLevelDBPath(): Promise<string> {
-    // Implement this method based on your application logic
     return 'path/to/leveldb'
   }
 
-  async defaultTaskSetup() {
+  async defaultTaskSetup(): Promise<void> {
     if (taskNodeAdministered) {
-      return await genericHandler('defaultTaskSetup')
+      await genericHandler('defaultTaskSetup')
     } else {
-      if (this.#testingTaskState) return Promise.resolve()
-      this.#testingMainSystemAccount = new Keypair()
-      this.#testingStakingSystemAccount = new Keypair()
-      this.#testingDistributionList = {}
-      this.#testingTaskState = {
-        task_name: 'DummyTestState',
-        task_description: 'Dummy Task state for testing flow',
+      if (this.testingTaskState) return
+      this.testingMainSystemAccount = new Keypair()
+      this.testingStakingSystemAccount = new Keypair()
+      this.testingDistributionList = {}
+      this.testingTaskState = {
         submissions: {},
-        submissions_audit_trigger: {},
-        total_bounty_amount: 10000000000,
-        bounty_amount_per_round: 1000000000,
-        total_stake_amount: 50000000000,
-        minimum_stake_amount: 5000000000,
-        available_balances: {},
-        stake_list: {},
-        round_time: 600,
-        starting_slot: 0,
-        audit_window: 200,
-        submission_window: 200,
-        distribution_rewards_submission: {},
-        distributions_audit_trigger: {},
       }
     }
   }
 }
 
-async function genericHandler(...args: string[]) {
+async function genericHandler(
+  ...args: string[]
+): Promise<GenericHandlerResponse> {
   try {
     const response = await axios.post(BASE_ROOT_URL, {
       args,
       taskId: TASK_ID,
       secret: SECRET_KEY,
     })
-    if (response.status == 200) return response.data.response
-    else {
+    if (response.status === 200) {
+      return response.data.response
+    } else {
       console.error(response.status, response.data)
-      return null
+      return { error: response.data }
     }
   } catch (err: any) {
     console.error(`Error in genericHandler: "${args[0]}"`, err.message)
@@ -189,6 +208,7 @@ async function genericHandler(...args: string[]) {
     return { error: err }
   }
 }
+
 const namespaceWrapper = new NamespaceWrapper()
 
 if (taskNodeAdministered) {
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,35 @@
-export interface Document {
-    key: string;
-    [key: string]: any;
-  }
-  
+import Datastore from 'nedb-promises'
+
+// Define types for the handler response and arguments
+
+export type GenericHandlerResponse = number | { error: any }
+
+export type Submission = {
+  submission_value: string
+  slot: number
+  round: number
+}
+
+// Define the interface for the task state
+export interface TaskState {
+  submissions: { [round: number]: { [publicKey: string]: Submission } }
+}
+
+// Define the interface for the class
+export interface TaskNode {
+  testingTaskState?: TaskState | null
+  testingStakingSystemAccount?: { publicKey: { toBase58(): string } } | null
+  initializeDB(): Promise<void>
+  getDb(): Promise<Datastore<Document>>
+  storeSet(key: string, value: string): Promise<void>
+  getSlot(): Promise<number>
+  checkSubmissionAndUpdateRound(
+    submissionValue: string,
+    round: number,
+  ): Promise<void>
+  storeGet(key: string): Promise<string | null>
+  getTaskState(options: any): Promise<any>
+  getRpcUrl(): Promise<string>
+  getTaskLevelDBPath(): Promise<string>
+  defaultTaskSetup(): Promise<void>
+}
