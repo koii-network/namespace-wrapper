8d3e074da247b06094ac1aa02d7061dc0319f6a3
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2024-12-11 12:25:39 -0400

    Extended the maximum request body size

--- src/index.ts
+++ src/index.ts
@@ -41,8 +41,8 @@ const app = express()
 
 console.log('SETTING UP EXPRESS')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
+app.use(bodyParser.json({ limit: '50mb' }))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
