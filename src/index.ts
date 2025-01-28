53625df62e6d769445e27727967400ea8e264594
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2025-01-28 18:32:32 -0400

    Added custom parameter for taskType in getSubmitterAccount

--- src/index.ts
+++ src/index.ts
@@ -593,9 +593,14 @@ class NamespaceWrapper implements TaskNode {
     }
   }
 
-  async getSubmitterAccount(): Promise<Keypair | null> {
+  async getSubmitterAccount(taskType?: string): Promise<Keypair | null> {
     if (taskNodeAdministered) {
-      const submitterAccountResp = await genericHandler('getSubmitterAccount')
+      let submitterAccountResp;
+      if (taskType) {
+        submitterAccountResp = await genericHandler('getSubmitterAccount', taskType)
+      } else {
+        submitterAccountResp = await genericHandler('getSubmitterAccount')
+      }
       return Keypair.fromSecretKey(
         Uint8Array.from(Object.values(submitterAccountResp._keypair.secretKey)),
       )
@@ -632,7 +637,7 @@ class NamespaceWrapper implements TaskNode {
       if (
         this.testingTaskState!.submissions_audit_trigger[round] &&
         this.testingTaskState!.submissions_audit_trigger[round][
-          candidatePubkey.toBase58()
+        candidatePubkey.toBase58()
         ]
       ) {
         this.testingTaskState!.submissions_audit_trigger[round][
@@ -956,7 +961,7 @@ class NamespaceWrapper implements TaskNode {
       if (
         this.testingTaskState!.distributions_audit_trigger[round] &&
         this.testingTaskState!.distributions_audit_trigger[round][
-          candidatePubkey.toBase58()
+        candidatePubkey.toBase58()
         ]
       ) {
         this.testingTaskState!.distributions_audit_trigger[round][
@@ -1160,8 +1165,8 @@ class NamespaceWrapper implements TaskNode {
       keys =
         keySets.length > 0
           ? [...keySets[0]].filter((key) =>
-              keySets.every((set) => set.has(key)),
-            )
+            keySets.every((set) => set.has(key)),
+          )
           : []
       if (keys.length == 0) {
         console.log('No common keys found in last 3 rounds')
